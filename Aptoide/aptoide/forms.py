c757bae8172f51b72d36e2ed1742729d098be0b4
Akinrinade <19569682+Akinrinade@users.noreply.github.com>
2019-05-04 00:53:09 +0100

    Update forms.py

--- Aptoide/aptoide/forms.py
+++ Aptoide/aptoide/forms.py
@@ -6,26 +6,7 @@ from selectable.forms import AutoCompleteSelectField, AutoComboboxSelectWidget
 from .lookups import MyLookup
 
 
-#
-# class myform(forms.ModelForm):
-#
-#     # choices=autocomplete.Select2ListChoiceField(choice_list=openmy_file,
-#     #     widget=autocomplete.ListSelect2(url='my-autocomplete')
-#     # )
-#     class Meta:
-#         model=my_options
-#         fields=('__all__')
-#         autocomplete_fields = 'app_options'
-#         widgets = {
-#             "app_options": autocomplete.ListSelect2(url='my-autocomplete'),
-#         }
 
 
 class myform(forms.Form):
     apps_options =AutoCompleteSelectField(lookup_class=MyLookup, widget=AutoComboboxSelectWidget)
-    # class Meta:
-        #
-        # fields = ('app_options',)
-        # widgets = {
-        #     'app_options': autocomplete.ListSelect2(url='my-autocomplete'),
-        # }
